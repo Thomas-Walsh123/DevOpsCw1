e428a741fe2d9ec446037ca9cc120b01c37f0f4e
Thomas-Walsh123 <tomwalsh678@gmail.com>
2024-11-12 18:16:03 +0000

    Updated code to return an error if no input is provided

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -4,6 +4,11 @@ class Dec2Hex
 {
 public static int Arg1;
     public static void main(String args[])    {
+
+	if (args.length == 0) {
+		System.out.println("ERROR: No Input Provided");
+		return;
+	}
         Arg1 = Integer.parseInt(args[0]);
         char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
         int rem, num;
